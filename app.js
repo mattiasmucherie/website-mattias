e2faf2c45aece127aceb7e65a69cfa9deddbc107
Mattias Mucherie <mattias.mattias.m@gmail.com>
2020-05-20 11:17:21 +0200

    add 418 prot

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
-
+app.get('/demo', (req,res) => {
+  res.set("X-full-stack", "4life");
+  res.status(418);
+  res.send("I prefer water");
+})
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
