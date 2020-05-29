8fe9041c60be643be0b5f5eed527f3b964a0ba51
Mattias Mucherie <mattias.mattias.m@gmail.com>
2020-05-29 17:54:53 +0200

    Add navbar and css

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express()
+const path = require("path")
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/demo', (req,res) => {
-  res.set("X-full-stack", "4life");
-  res.status(418);
-  res.send("I prefer water");
-})
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')))
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
